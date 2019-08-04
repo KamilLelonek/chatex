29c2ecd82888cbee2746726a75fda077621f344d
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-04 20:21:34 +0200

    Test Message storaage

--- lib/chatex/domain.ex
+++ lib/chatex/domain.ex
@@ -1,5 +1,15 @@
 defmodule Chatex.Domain do
+  alias Chatex.Domain.Message.Mutator
+  alias Chatex.Domain.ErrorTranslator
+
   def messages(_conversation_id) do
     []
+  end
+
+  def store_message(payload) do
+    case Mutator.create(payload) do
+      {:ok, message} -> {:ok, message}
+      {:error, changeset} -> {:error, ErrorTranslator.call(changeset)}
+    end
   end
 end
--- test/chatex/domain_test.exs
+++ test/chatex/domain_test.exs
@@ -1,3 +1,28 @@
 defmodule Chatex.DomainTest do
   use Chatex.TestCase, async: true
+
+  alias Chatex.Domain
+
+  describe "store_message/1" do
+    test "should store and return a Message" do
+      payload = Factory.params_for(:message)
+
+      assert {
+               :ok,
+               %Chatex.Domain.Message.Schema{
+                 body: _,
+                 conversation_id: _,
+                 sender: _,
+                 inserted_at: _,
+                 updated_at: _
+               }
+             } = Domain.store_message(payload)
+    end
+
+    test "should not store a Message and return an error" do
+      payload = Factory.params_for(:message, sender: 1)
+
+      assert {:error, %{sender: ["is invalid"]}} = Domain.store_message(payload)
+    end
+  end
 end
