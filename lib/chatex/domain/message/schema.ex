d6180de990df75bec67a32b5df1beeb49a814d13
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-12 19:08:55 +0200

    Extend Message Schema

--- lib/chatex/domain/message/schema.ex
+++ lib/chatex/domain/message/schema.ex
@@ -2,12 +2,14 @@ defmodule Chatex.Domain.Message.Schema do
   use Chatex.Domain.Schema
 
   alias Chatex.Domain.Conversation.Schema, as: Conversation
+  alias Chatex.Domain.View.Schema, as: View
 
   schema "messages" do
     field :body, :string
     field :sender, :string
 
     belongs_to :conversation, Conversation
+    has_many :views, View, foreign_key: :id
 
     timestamps()
   end
