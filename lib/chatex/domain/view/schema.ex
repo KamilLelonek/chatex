e2b1c8fc566a7f6ed7a553777568341e3668092b
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-12 19:08:43 +0200

    Create Views

---
+++ lib/chatex/domain/view/schema.ex
@@ -0,0 +1,13 @@
+defmodule Chatex.Domain.View.Schema do
+  use Chatex.Domain.Schema
+
+  alias Chatex.Domain.Message.Schema, as: Message
+
+  schema "views" do
+    field :reader, :string
+
+    belongs_to :message, Message
+
+    timestamps()
+  end
+end
---
+++ priv/repo/migrations/20190810161234_create_views.exs
@@ -0,0 +1,16 @@
+defmodule Chatex.Domain.Repo.Migrations.CreateViews do
+  use Ecto.Migration
+
+  @table :views
+
+  def change do
+    create table(@table) do
+      add :message_id, references(:messages, on_delete: :delete_all)
+      add :reader, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(@table, [:message_id, :reader])
+  end
+end
